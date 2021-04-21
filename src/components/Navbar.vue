3f223dd300112cc413af2d7ab78da8d28498034a
Augusto <augusto.magar@gmail.com>
2021-04-20 23:21:39 -0300

    Add menu lateral retratio

--- src/components/Navbar.vue
+++ src/components/Navbar.vue
@@ -1,7 +1,10 @@
 <template>
-    <div class="componentNav">
+    <div class="container" :class="{'show': showSidebar}">
+        <div class="control">
+            <i class="fas fa-angle-double-right" @click="showNav"></i>
+        </div>
+        <!--
         <div class="container columns is-desktop is-centered">
-            <!-- <div class="column is-full-mobile"> -->
             <div class="column">
                 <div class="logo">
                     <div class="">
@@ -9,91 +12,137 @@
                     </div>
                 </div>
             </div>
-            <!-- <div class="column is-8 ">
-                <h1 class="title">LabSystem</h1>
-            </div> -->
         </div>
-
-        <div class="container opcoes">
-            <ul>
-                <li><router-link to="/home">
-                    <i class="fas fa-home"><span>Início</span></i> 
-                </router-link></li>
-                <li><router-link to="/directory">
-                    <i class="fas fa-folder-open"><span>Diretório</span></i>
-                </router-link></li>
-                <li><router-link to="/profile">
-                    <i class="fas fa-user-circle"><span>Meus dados</span></i>
-                </router-link></li>
-                <li><router-link to="/login">
-                    <i class="fas fa-door-open"><span>Sair</span></i>
-                </router-link></li>
-            </ul>
+        <div class="navigation-icons">
+            <i class="fas fa-home"></i>
+            <i class="fas fa-folder-open"></i>
+            <i class="fas fa-user-circle"></i>
+            <i class="fas fa-door-open"></i>
+        </div>-->
+        <div class="navigation-links">
+            <transition-group name="fade">
+                <ul>
+                    <div v-show="showLink" key="1">
+                        <li><router-link to="/home">
+                            <i>Início</i> 
+                        </router-link></li>
+                    </div>
+                    <div v-show="showLink" key="2">
+                        <li><router-link to="/directory">
+                            <i>Diretório</i>
+                        </router-link></li>
+                    </div>
+                    <div v-show="showLink" key="3">
+                        <li><router-link to="/profile">
+                            <i>Meus Dados</i>
+                        </router-link></li>
+                    </div>
+                    <div v-show="showLink" key="4">
+                        <li><router-link to="/login">
+                            <i>Sair</i>
+                        </router-link></li>
+                    </div>
+                </ul>
+            </transition-group>
         </div>
     </div>
 </template>
 
 <script>
-export default {
-    name:'Navbar',
-    methods:{
-        fechar(){
-            console.log('x')
+  export default {
+    data: () => {
+      return {
+        showSidebar: false,
+        showLink: false
+      }
+    },
+    methods: {
+      showNav() {
+        if(this.showSidebar) {
+          this.showLink = false;
+          setTimeout(() => {
+            this.showSidebar = false;
+          }, 500);
         }
-
+        else {
+          this.showSidebar = true;
+          setTimeout(() => {
+            this.showLink = true;
+          }, 500);
+        }
+      }
     }
-
-    }
+  }
 </script>
 
 <style scoped>
-
-.componentNav{
+.container {
     position: relative;
-    /* width: 100% !important; */
-    height: 100%;
-    background-color: rgb(255, 255, 255);
-    box-shadow: 0 0 10px rgb(116, 116, 116);
-    border: 1px solid white;
+    top: 0;
+    left: 0;
+    width: 70px;
+    padding: 10px;
+    min-height: calc(100vh - 20px);
+    background-color: rgb(175, 174, 174);
+    border: solid rgb(97, 97, 97);
+    border-width: 0 1px 0 0;
+    z-index: 999;
+    transition: all .5s ease-in-out;
 }
-.title{
-    color: var(--cor-da-letra);
-    margin-top: 20%;
-    width: 100%;
-}
-.logo{
-    height: 150px;
-    padding: 15%;
-    display: flex;
-    justify-content: center;
-}
-.opcoes{
-    padding: 10% 0 0 5%;
-}
-
-.opcoes ul li{
-    list-style-type: none;
-    width: 100%;
-    margin: 8%;
-}
-i{
-    color: var(--cor-da-letra);
-    width: 100%;
-}
-i:hover{
-    color: slateblue
-}
-.opcoes ul li a span{
-    width: 100%;
-    margin-left:5%;
-    color: var(--cor-da-letra);
-    text-decoration: none;
-    cursor: pointer;
-    font-family: 'Dosis', sans-serif;
-    font-size: 1.5rem;
-    font-weight: 700;
-}
-.opcoes ul li a span:hover{
-    color: slateblue;
-}
+    .control {
+      color: var(--cor-da-letra);
+      display: flex;
+      justify-content: center;
+      align-items: center;
+      width: 50px;
+      margin-bottom: 10px;
+    }
+      i {
+        font-size: 2rem;
+        cursor: pointer;
+        transition: all .5s ease-in-out;
+      }
+    
+    .show {
+      width: 200px;
+    }
+    
+      /* .control > i {
+        color: #fff;
+        transform: rotateZ(-180deg);
+      }*/
+      .navigation-icons {
+        color: #fff;
+      }
+    
+    .navigation-icons {
+      display: flex;
+      justify-content: center;
+      align-items: center;
+      flex-direction: column;
+      width: 50px;
+      float: left;
+    }
+      i {
+        font-size: 2rem;
+        padding: 10px 0;
+        cursor: pointer;
+        transition: all .5s ease-in-out;
+      }
+        i:hover {
+          color: #fff;
+        }
+    .navigation-links {
+      padding-top: 14px;
+      float: left;
+    }
+      div {
+        font-size: 1.35rem;
+        padding-left: 10px;
+        margin-bottom: 18px;
+        cursor: pointer;
+      }
+        div:hover {
+          color: #fff;
+        } 
 </style>
