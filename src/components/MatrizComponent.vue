72c8a94907f9d9098050a36d62eac4767950633f
Augusto <augusto.magar@gmail.com>
2021-04-21 12:39:46 -0300

    Atualização de Nova Amostra

---
+++ src/components/MatrizComponent.vue
@@ -0,0 +1,107 @@
+<template>
+<div class="card">
+    <table class="table is-fullwidth">
+        <thead>
+            <th>#</th>
+            <th>Componente</th>
+            <th>Concentração</th>
+        </thead>
+        <tbody v-for="elemento, index in amostraRascunho.elementos" :key="index">
+            <tr>
+                <th>{{index +1}}</th>
+                <th>
+                    <div class="field">
+                        <div class="control">
+                            <input class="input" type="text" placeholder="Componente">
+                        </div>
+                    </div>
+                </th>
+                <th>
+                    <div class="field">
+                        <div class="control">
+                            <input class="input" type="text" placeholder="%">
+                        </div>
+                    </div>
+                </th>
+            </tr>
+        </tbody>
+        <tfoot>
+            <tr>
+                <th>
+
+                </th>
+                <th>
+                    Adicione ou remova Componente
+                </th>
+                <th>
+                    <div class="field is-grouped">
+                        <div class="control">
+                            <button class="button is-link is-rounded" @click="adicionaElemento">+</button>
+                        </div>
+                        <div class="control">
+                            <button class="button is-danger is-rounded" @click="removeElemento">-</button>
+                        </div>
+
+                    </div>
+                </th>
+            </tr>
+        </tfoot>
+
+    </table>
+</div>
+</template>
+
+<script>
+export default {
+    data(){
+        return{
+            amostraRascunho:{
+                titulo:'',
+                descricao:'',
+                elementos:[
+                    {
+                        componente:'',
+                        agente:''
+                    },
+                    {
+                        componente:'',
+                        agente:''
+                    },
+                    {
+                        componente:'',
+                        agente:''
+                    },
+                    {
+                        componente:'',
+                        agente:''
+                    },
+                ]
+            }
+        }
+    },
+    methods:{
+        adicionaElemento(){
+            this.amostraRascunho.elementos.push({
+                        
+                        componente:'',
+                        agente:''
+                    })
+        },
+        removeElemento(){
+            this.amostraRascunho.elementos.pop({
+                        componente:'',
+                        agente:''
+                    })
+        }
+    }
+}
+</script>
+
+<style scoped>
+hr{
+    background-color: rgba(214, 214, 214, .5);
+}
+.salvar{
+    margin-top: 2%;
+}
+</style>
